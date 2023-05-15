78af46ef67391ea8f0d2c390585b1181f3ea8f8f
NoorAhmed786 <aniiahmed380@gmail.com>
2023-05-15 23:54:57 +0500

    condition

---
+++ app.js
@@ -0,0 +1,45 @@
+// //qustn 1
+// var A=65;
+// var Z=90; 
+// var a=97 ;
+// var z=122;
+
+// var userInput=prompt("enter a numer or string");
+// if (userInput === A)
+
+// //qustn 3
+// var num=parseInt(prompt("enter a number"));
+// if()
+
+//qustn 5
+// var correctPassword="noor123";
+// var userInput1=prompt("tell me a password ");
+// var userInput2=prompt("tell me a password ");
+// var user= userInput1+ userInput2;
+// if( user!== 0 ){
+//     alert("please enter your password");
+
+// }else if(userInput1 === userInput2){
+//     alert("Correct! The password you entered matches the original password. Show Incorrect password” otherwise");
+// }
+
+//qustn 6
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+// qustn 2
+var num1=parseInt(prompt("enter a number"));
+var num2=parseInt(prompt("enter a number"));
+var num3=num1+num2;
+if(num1>=num2 && num1 ===num2){
+    alert(num3);
+}
+
+
+
